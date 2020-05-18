c3476c0f307e783e3436e25994fe81144bd57561
Marcel Poisot <198049+mpoisot@users.noreply.github.com>
2020-05-18 12:42:24 -0500

    Don't fail prod build if typescript errors

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL || "http://localhost:8008",
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
